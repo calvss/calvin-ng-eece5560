fe9378195059991b0c408a6873cbd19e88c9cbfe
calvss <calvin.alexander.ng@gmail.com>
2022-02-02 21:14:17 -0500

    change msg type

--- packages/calvin_hw2/src/UnitsConverter.py
+++ packages/calvin_hw2/src/UnitsConverter.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 
 import rospy
-from std_msgs.msg import Float32
 from mystery_package.msg import UnitsLabelled
 
 class UnitsConverter:
     def __init__(self):
-        rospy.Subscriber('/mystery/output2', Float32, self.callback)
+        rospy.Subscriber('/mystery/output2', UnitsLabelled, self.callback)
 
     def callback(self, msg):
         units = msg.units
