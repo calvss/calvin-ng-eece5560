e5a6af962011475d9c74a029ad721367445f33f0
calvss <calvin.alexander.ng@gmail.com>
2022-02-02 21:31:09 -0500

    Unit conversion and publishing

--- packages/calvin_hw2/src/UnitsConverter.py
+++ packages/calvin_hw2/src/UnitsConverter.py
@@ -7,10 +7,16 @@ class UnitsConverter:
     def __init__(self):
         rospy.Subscriber('/mystery/output2', UnitsLabelled, self.callback)
 
+        self.publisher = rospy.Publisher('outputFeet', UnitsLabelled, queue_size=10)
+        self.pub_msg = UnitsLabelled()
+        self.pub_msg.units = "feet"
+
     def callback(self, msg):
-        units = msg.units
-        value = msg.value
-        rospy.loginfo("I got " + str(units) + " units and " + str(value) + " data")
+        metersPerFoot = 0.3048
+        self.meters = msg.value
+        self.feet = self.meters / metersPerFoot
+        self.pub_msg.value = self.feet
+        self.publisher.publish(self.pub_msg)
 
 if __name__ == '__main__':
     rospy.init_node('UnitsConverter')
