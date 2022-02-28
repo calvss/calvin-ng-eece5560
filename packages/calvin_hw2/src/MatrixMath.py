b5680a9018b534486c89547cfe6bef23dfc25512
calvss <calvin.alexander.ng@gmail.com>
2022-02-28 12:55:27 -0500

    add world coordinates

--- packages/calvin_hw2/src/MatrixMath.py
+++ packages/calvin_hw2/src/MatrixMath.py
@@ -21,16 +21,26 @@ class MatrixMath:
                          [np.sin(np.radians(135)),      np.cos(np.radians(135)), 2],
                          [                      0,                            0, 1]])
 
+        tf_sensorWorld = tf_robotWorld.dot(tf_sensorRobot)
+
         sensorCoord = np.array([[0], [0], [1]])
         sensorCoord[0] = [msg.x]
         sensorCoord[1] = [msg.y]
-        robotCoord = tf_sensorRobot * sensorCoord
 
+        robotCoord = tf_sensorRobot.dot(sensorCoord)
         v_robotCoord = Vector2D()
         v_robotCoord.x = robotCoord[0][0]
         v_robotCoord.y = robotCoord[1][0]
 
+        worldCoord = tf_sensorWorld.dot(sensorCoord)
+        v_worldCoord = Vector2D()
+        v_worldCoord.x = worldCoord[0][0]
+        v_worldCoord.y = worldCoord[1][0]
+
         self.publisherRobot.publish(v_robotCoord)
+        self.publisherWorld.publish(v_worldCoord)
+
+
 
 if __name__ ==  '__main__':
     rospy.init_node('MatrixMath')
