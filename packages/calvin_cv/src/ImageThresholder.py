df4ac5748fc2bb46caef0fb387e801c7fc4577df
calvss <calvin.alexander.ng@gmail.com>
2022-03-29 17:58:58 -0400

    fix morph filtering

--- packages/calvin_cv/src/ImageThresholder.py
+++ packages/calvin_cv/src/ImageThresholder.py
@@ -47,7 +47,7 @@ class ImageThresholder:
         cleanImage = cv2.morphologyEx(HSVImage, cv2.MORPH_CLOSE, element)
 
         element = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10, 10))
-        cleanImage = cv2.morphologyEx(HSVImage, cv2.MORPH_OPEN, element)
+        cleanImage = cv2.morphologyEx(cleanImage, cv2.MORPH_OPEN, element)
 
         whiteImage = cv2.inRange(cleanImage, (self.WHITE["hueL"], self.WHITE["satL"], self.WHITE["valL"]), (self.WHITE["hueH"], self.WHITE["satH"], self.WHITE["valH"]))
         yellowImage = cv2.inRange(cleanImage, (self.YELLOW["hueL"], self.YELLOW["satL"], self.YELLOW["valL"]), (self.YELLOW["hueH"], self.YELLOW["satH"], self.YELLOW["valH"]))
