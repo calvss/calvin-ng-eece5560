847b7bab2a93418955055424e5e85e106a4f99eb
calvss <calvin.alexander.ng@gmail.com>
2022-04-03 23:24:07 -0400

    HW8 compliance

--- packages/calvin_cv/src/EdgeDetector.py
+++ packages/calvin_cv/src/EdgeDetector.py
@@ -14,7 +14,7 @@ class EdgeDetector:
         rospy.Subscriber('/image_yellow', Image, self.yellowCallback)
         rospy.Subscriber('/image_white', Image, self.whiteCallback)
         rospy.Subscriber('/image_cropped', Image, self.croppedCallback)
-        self.cropPublisher = rospy.Publisher("/image_edges", Image, queue_size = 10)
+        self.edgesPublisher = rospy.Publisher("/image_edges", Image, queue_size = 10)
         self.yellowPublisher = rospy.Publisher("/image_lines_yellow", Image, queue_size = 10)
         self.whitePublisher = rospy.Publisher("/image_lines_white", Image, queue_size = 10)
 
@@ -24,12 +24,12 @@ class EdgeDetector:
 
     def yellowCallback(self, msg):
         image = self.bridge.imgmsg_to_cv2(msg, "mono8")
-        houghLines = self.getHough(image, 2)
+        houghLines = self.getHough(image, 5)
         self.yellowEdges.put(houghLines)
 
     def whiteCallback(self, msg):
         image = self.bridge.imgmsg_to_cv2(msg, "mono8")
-        houghLines = self.getHough(image, 2)
+        houghLines = self.getHough(image, 15)
         self.whiteEdges.put(houghLines)
 
     def croppedCallback(self, msg):
@@ -47,19 +47,17 @@ class EdgeDetector:
         ros_white_edges = self.bridge.cv2_to_imgmsg(whiteLines, "bgr8")
         ros_yellow_edges = self.bridge.cv2_to_imgmsg(yellowLines, "bgr8")
 
-        #self.cropPublisher.publish(ros_all_edges)
+        self.edgesPublisher.publish(ros_all_edges)
         self.whitePublisher.publish(ros_white_edges)
         self.yellowPublisher.publish(ros_yellow_edges)
 
-    def getHough(self, image, thickness):
+    def getHough(self, image, length):
         #element = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (thickness, thickness))
         #binaryEdges = cv2.morphologyEx(image, cv2.MORPH_GRADIENT, element)
 
         binaryEdges = cv2.Canny(image, 100, 200)
-        ros_all_edges = self.bridge.cv2_to_imgmsg(binaryEdges, "mono8")
-        self.cropPublisher.publish(ros_all_edges)
 
-        houghLines = cv2.HoughLinesP(binaryEdges, 1, 3.14159/180, 20, None, 15, 10)
+        houghLines = cv2.HoughLinesP(binaryEdges, 1, 3.14159/180, 15, None, length, 10)
         return houghLines
 
     def drawLines(self, image, lines):
