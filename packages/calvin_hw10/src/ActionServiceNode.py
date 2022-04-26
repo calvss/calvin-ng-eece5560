4e1717c5df76beb11e960a0881f69750cbf4f585
calvss <calvin.alexander.ng@gmail.com>
2022-04-26 17:31:38 -0400

    add timing

--- packages/calvin_hw10/src/ActionServiceNode.py
+++ packages/calvin_hw10/src/ActionServiceNode.py
@@ -3,16 +3,20 @@
 import rospy
 from example_service.srv import *
 
-def fibonacciClient(x):
+def fibonacciClientTimer(x):
     rospy.wait_for_service('calc_fibonacci')
     try:
         fibonacciProxy = rospy.ServiceProxy('calc_fibonacci', Fibonacci)
+
+        startTime = rospy.get_rostime()
         serviceResponse = fibonacciProxy(x)
-        return serviceResponse
+        elapsedTime = rospy.get_rostime() - startTime
+        return elapsedTime
     except rospy.ServiceException as e:
         print("Service call failed: %s", e)
 
 if __name__ == '__main__':
     rospy.init_node('ActionServiceNode')
-    rospy.logwarn(fibonacciClient(3))
+    rospy.logwarn(fibonacciClientTimer(3))
+    rospy.logwarn(fibonacciClientTimer(15))
     rospy.spin()
